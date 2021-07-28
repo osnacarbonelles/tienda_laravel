30403f434f3e7ce8eb08ce2b52f29f22a9537e59
Osnaider Carbonell <42350856+osnacarbonelles@users.noreply.github.com>
2021-07-27 23:04:05 -0500

    agregando la funcion agregar en el controlador del carrito

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,7 +16,21 @@ class CartController extends Controller
 
     public function cart()  {
         $cartCollection = \Cart::getContent();
-        dd($cartCollection);
+        // dd($cartCollection);
         return view('cart')->withTitle('E-COMMERCE STORE | CART')->with(['cartCollection' => $cartCollection]);;
+    }
+
+    public function add(Request $request){
+        \Cart::add(array(
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'attributes' => array(
+                'image' => $request->img,
+                'slug' => $request->slug
+            )
+        ));
+        return redirect()->route('cart.index')->with('success_msg', 'Item is aggregate!');
     }
 }
